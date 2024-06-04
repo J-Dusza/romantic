eb3170c01f37a1b87a6d3fbd8178a9a0432a3e1a
Jan Dusza <duszajan@icloud.com>
2024-06-04 23:25:15 +0200

    navbar template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   metadataBase: new URL("https://postgres-prisma.vercel.app"),
