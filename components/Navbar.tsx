d7dabebbd31b794cba13cc5b45c14aeadc301250
Jan Dusza <duszajan@icloud.com>
2024-06-05 00:24:02 +0200

    roms

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            ROMance
+            ROMantic
           </span>
         </a>
         <div className="flex md:order-2">
