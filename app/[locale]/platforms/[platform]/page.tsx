4458df4e49ac5391b70d3699c771d711a23c1d7a
Jan Dusza <duszajan@icloud.com>
2024-06-07 12:59:21 +0200

    platform desc localized

--- app/[locale]/platforms/[platform]/page.tsx
+++ app/[locale]/platforms/[platform]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getPlatformByShortname } from "@/controller/platformsController";
 import RomsGallery from "@/components/RomsGallery";
 import { getRomsByPlatform } from "@/controller/romsController";
+import initTranslations from "@/app/i18n";
 
 type PlatformProps = {
   params: {
@@ -10,14 +11,15 @@ type PlatformProps = {
   };
 };
 
-async function Platform({ params: { platform } }: PlatformProps) {
+async function Platform({ params: { locale, platform } }: PlatformProps) {
   const platformData = await getPlatformByShortname(platform);
   const roms = await getRomsByPlatform(platform);
+  const { t } = await initTranslations(locale, ["Platforms"]);
 
   return (
     <div className="p-5 m-10 min-h-screen">
       <h1 className="text-3xl font-bold mb-4">{platformData?.name}</h1>
-      <p className="mb-8">{platformData?.description}</p>
+      <p className="mb-8">{t(`description_${platform.toLowerCase()}`)}</p>
       <RomsGallery roms={roms} />
     </div>
   );
